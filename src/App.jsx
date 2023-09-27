1f478f3d746fd1e3508b04dc853c01ddbc76f4cc
Kaspar Marnat <kasparmarnat@gmail.com>
2023-09-27 16:04:22 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      <header>header</header>\
+      <main>
+        <Outlet />
+      </main>
+      <footer>footer</footer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import './index.scss';
+import Homepage from './pages/Homepage/Homepage';
+import ContactPage from './pages/ContactPage/ContactPage';
+import SignupPage from './pages/SignupPage/SignupPage';
+
+const router = createBrowserRouter([
+  {
+    path: '',
+    element: <App />,
+    children: [
+      {
+        path: '/home',
+        element: <Homepage />,
+      },
+      {
+        path: '/signup',
+        element: <SignupPage />,
+      },
+      {
+        path: '/contact',
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider route={router} />
+  </React.StrictMode>
+);
